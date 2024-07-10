12f5d45e8d77b4b9083b07c22ff366d91b54a0a7
HsiaoCz <984274788@qq.com>
2024-07-11 07:36:11 +0800

    add some

--- choose/client/cmd/main.go
+++ choose/client/cmd/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/HsiaoCz/beast-clone/choose/client"
+)
 
 func main() {
-	fmt.Println("all is well")
+	client := client.NewClient("http://localhost:9001/price")
+	responsePrice, err := client.FetchPrice(context.Background(), "GG")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("response %v\n", responsePrice)
 }
