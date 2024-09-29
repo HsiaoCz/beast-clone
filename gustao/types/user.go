437062d5ddad8a28f14f4eed96e6b2260e545a47
HsiaoCz <984274788@qq.com>
2024-09-29 18:20:40 +0800

    add some

--- gustao/types/user.go
+++ gustao/types/user.go
@@ -1,4 +1,12 @@
 package types
 
+import "gorm.io/gorm"
 
-type User struct{}
+type User struct {
+	gorm.Model
+	User_ID       string `gorm:"column:user_id;" json:"user_id"`
+	Username      string `gorm:"column:username;" json:"username"`
+	Email         string `gorm:"column:email" json:"email"`
+	User_Password string `gorm:"column:user_password" json:"user_password"`
+	
+}
